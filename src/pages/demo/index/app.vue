13eba0fcc42fee1b2d91e47bcf4cfaac4f641400
JasonFF <dfQ_Qjb@qq.com>
2018-01-11 15:57:50 +0800

    my tool

---
+++ src/pages/demo/index/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <Button>demo</Button>
+  </div>
+</template>
+
+<script>
+import logo from 'assets/img/logo.png'
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!',
+      logoImg: logo
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="postcss">
+  body {
+    background-color: #f5f5f5;
+  }
+  :root h1 {
+   --color: red;
+   display: flex;
+   color: var(--color);
+  }
+</style>
